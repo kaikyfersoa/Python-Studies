4aae1f4d529ee7e47619c3f76dd30627312cc89b
Kaiky <135863012+kaikyfersoa@users.noreply.github.com>
2023-08-02 22:28:03 -0300

    Update exercício3.py

--- Python-Studies/aula 9/exercício3.py
+++ Python-Studies/aula 9/exercício3.py
@@ -1,10 +1,10 @@
 # Escreva uma funcção que receba uma lista de palavras  como
 # entrada e retorne uma lista apenas com palavras que tem mais de 5 letras
 
-dic = {"nomes": 0}
-nomes = ["Rodrigo", "Maria", "Mariana", "Juliana", "João"]
+def palavras_mais_de_5_letras(lista_palavras):
+    return [palavra for palavra in lista_palavras if len(palavra) > 5]
 
-def nome5 (x, y):
-    dic = {x:y}
-    d1 = {x: y for x, y in  dic}
-    
+# Exemplo de uso:
+lista_palavras = ["python", "linguagem", "programar", "gato", "elefante", "casa"]
+palavras_com_mais_de_5_letras = palavras_mais_de_5_letras(lista_palavras)
+print(palavras_com_mais_de_5_letras)
